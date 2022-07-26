35c39a73709287b75b258f1ed860b59cc8053b87
enclar <107608010+enclar@users.noreply.github.com>
2022-07-26 22:05:56 +0800

    trying to get shooter to move in correct direction and distance

--- puzzle-bobble/main.js
+++ puzzle-bobble/main.js
@@ -16,14 +16,17 @@ const addBubble = () =>  {
 };
 
 //! Creating a bubble class
-class Bubble {
-  constructor(row, col, coords, color) {
-    this.row = row;
-    this.col = col;
-    this.coords = coords;
-    this.color = color;
-  };
-};
+// class Bubble {
+//   constructor(row, col, coords, color) {
+//     this.row = row;
+//     this.col = col;
+//     this.coords = coords;
+//     this.color = color;
+//   }
+//   getColor() {
+//     return this.color
+//   }
+// };
 
 //! Creating an array to hold the coordinates of each bubble generated
 const bubbleGrid = [];
@@ -41,14 +44,16 @@ const addEvenRow = (i, j) => { // Function takes row & col number as a parameter
   $bubble.attr("data-row", i); // Setting row data
   $bubble.attr("data-col", j); // Setting column data
 
-  bubbleGrid.push(new Bubble(i, j, [(i*50), (j*50)], $bubbleColor)); // Appending bubble data to bubble grid array
+  // bubbleGrid.push(new Bubble(i, j, [(i*50), (j*50)], $bubbleColor)); // Appending bubble data to bubble grid array
+
+  bubbleGrid.push([i, j]);
 
   $(".row").eq(i).append($bubble); // Adding the bubble into i row of grid
 
 };
 
 //! Creating a function to add in a bubble in an odd row
-const addOddRow = (i, j) => { // Function takes row number as a parameter
+const addOddRow = (i, j) => { // Function takes row & col number as a parameter
   const $bubble = addBubble();
 
   const $bubbleColor = randomColor(colors);
@@ -60,8 +65,10 @@ const addOddRow = (i, j) => { // Function takes row number as a parameter
   $bubble.attr("data-row", i); // Setting row data
   $bubble.attr("data-col", j); // Setting column data
 
-  bubbleGrid.push(new Bubble(i, j, [(i*50), (j*50)], $bubbleColor)); // Appending bubble data to bubble grid array
+  // bubbleGrid.push(new Bubble(i, j, [(i*50), (j*50)], $bubbleColor)); // Appending bubble data to bubble grid array
   
+  bubbleGrid.push([i, j]);
+
   $(".row").eq(i).append($bubble); // Adding the bubble into i row of grid
 };
 
@@ -96,34 +103,88 @@ const rotateTrajectory = (angle) => {
   $("#trajectory").css("transform", `rotate(${angle}deg)`);
 };
 
-//! Function to shoot bubble
-const shootBubble = () => {
-  $("#shooter-bubble ").animate({
-    left: "+=50",
-    bottom: "+=200"
-  });
-};
-
-//! Function to check where the bubble should stop
+//! Function to check which is the lowest row that the bubble can land in
 const $rows = $(".row");
 
 const firstEmptyDiv = () => { // Checking the lowest row which contains bubbles
-  let lowestRow = 8;
-  for (let i=0; i<9; i++) {
+  let lowestRow = 8; // Assuming the lowest row is 8
+  for (let i=0; i<9; i++) { // For loop to check if each row has children
     if ($rows.eq(i).children().length === 0) {
-      return lowestRow = i-1;
+      return lowestRow = i; 
     };
   };
 };
 
+//! Creating an array to hold the possible intersections
 let possibleIntersections = [];
 
+//! Function to convert angle from degrees to radians
+const convertAngle = (angle) => {
+  if (angle < 0) {
+    angle = -(angle);
+  }
+  return angle * (Math.PI / 180);
+};
 
+//! Creating function to find possible launch positions
+const possiblePositions = (lowestRow) => {
+  const angleInRadians = convertAngle(rotateAngle); // Converting angle to radians
 
-// check if array has a bubble of those row/col coords
-// const shooterPosition = () => {
-//   const colToCheck = 
-// }
+  for (let i=0; i<=lowestRow; i++) {
+    const yDist = (9-i) * 50;
+    let xDist, col;
+    if (rotateAngle >= 0) { // If bubble is shooting towards the right
+      xDist = 250 + (yDist * Math.tan(angleInRadians));
+      col = Math.round(xDist/50);
+      console.log("angle is", rotateAngle);
+      console.log("yDist is", yDist);
+      console.log("xDist is", xDist);
+      console.log("xDist/50 is", xDist/50);
+      possibleIntersections.push([i, col]);
+    } else { // If bubble is shooting towards the left
+      xDist = 250 - (yDist * Math.tan(angleInRadians));
+      col = Math.round(xDist/50);
+      possibleIntersections.push([i, col]);
+    };
+  };
+
+  console.log(possibleIntersections);
+};
+
+//! Function to check if position is empty
+const finalPosition = (arr) => {
+  let trajectory;
+  for (const position of arr) {
+    if (!bubbleGrid.includes(position)) { // Checking if the div is filled
+      trajectory = position; // The lowest div that is not filled will be the final position
+    };
+  };
+
+  const bottom = (trajectory[0]-1) * 50;
+  let left;
+  console.log(trajectory[0], trajectory [1]);
+
+  if (trajectory[0]%2 === 0) { // If it is an even row
+    left = (trajectory[1] * 50) - 250;
+  } else { // If it is an odd row
+    left = (trajectory[1] * 50) - 250;
+  };
+
+  shootBubble(left, bottom);
+
+  // const $shooter = $("#shooter-bubble");
+  // $shooter.remove();
+  // $rows.eq(row).append($shooter.attr("id", ""))
+}
+
+//! Function to shoot bubble
+const shootBubble = (left, bottom) => {
+  $("#shooter-bubble ").animate({
+    left: "+=" + left,
+    bottom: "+=" + bottom
+  });
+};
+
 
 //! LOAD AFTER DOM HAS LOADED
 $(() => {
@@ -141,13 +202,13 @@ $(() => {
   $(window).on("keydown", (event) => {
     if (event.which === 39) { // Listening for pressdown of right arrow key
       if (rotateAngle <= 45) { // Preventing arrow from rotating out of the playing field
-        rotateAngle += 10; // Angle of rotation changes by 10deg with each press
+        rotateAngle += 1; // Angle of rotation changes by 10deg with each press
         rotateTrajectory(rotateAngle);
       };
     };
     if (event.which === 37) {
       if (rotateAngle >= -45) {
-        rotateAngle -= 10;
+        rotateAngle -= 1;
         rotateTrajectory(rotateAngle);
       };
     };
@@ -156,7 +217,9 @@ $(() => {
   //! Event listener to shoot
   $(window).on("keydown", (event) => {
     if (event.which === 32) { // Listening for pressdown of spacebar
-      shootBubble(); // Shooting the bubble
+      possiblePositions(firstEmptyDiv());
+      finalPosition(possibleIntersections);
+      // shootBubble(); // Shooting the bubble
       // getShooter(); // Generating a new shooter bubble
       rotateAngle = 0; // Resetting the trajectory angle to zero
       rotateTrajectory(rotateAngle); // Resetting trajectory div to center
